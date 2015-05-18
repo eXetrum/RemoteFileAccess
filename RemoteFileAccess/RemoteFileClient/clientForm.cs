a8bdf773d7b5aa0991fabb3bd8b1ebd412ba3349
eXetrum <shishimorovmaksim@gmail.com>
2015-05-18 16:20:59 +0300

    client/server edit done

--- RemoteFileAccess/RemoteFileClient/clientForm.cs
+++ RemoteFileAccess/RemoteFileClient/clientForm.cs
@@ -16,6 +16,7 @@ namespace RemoteFileClient
 {
     public partial class clientForm : Form
     {
+        // 
         Client client;
         public clientForm()
         {
@@ -29,27 +30,37 @@ namespace RemoteFileClient
             {
                 try
                 {
+                    // Создаем точку подключения, взяв айпи аддресс и порт из текстовых полей
                     IPEndPoint ep = new IPEndPoint(IPAddress.Parse(txt_server_address.Text), int.Parse(txt_server_port.Text));
+                    // Создаем потоковый сокет используюя протокол TCP
                     Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
+                    // Соединяемся с удаленным аддрессом
                     socket.Connect(ep);
+                    // Если соединение успешно
                     if (socket.Connected)
                     {
+                        // Выключаем кнопку "соединиться"
                         btn_connect.Enabled = false;
+                        // Включаем кнопку "отсоединиться"
                         btn_disconnect.Enabled = true;
+                        // Включаем кнопку поиска файла
                         btn_search.Enabled = true;
+                        // Включаем поле ввода для поиска файла
                         txt_file_name.Enabled = true;
+                        // Создаем объект соединения, который облегчит обработку отправки данных и приема ответов от сервера
                         client = new Client(socket);
-
+                        // Создаем пустой пакет, и укажим тип сообщения "LIST" - попросим сервер вернуть содержимое текущего каталога
                         Packet p = new Packet(MSG_TYPE.LIST, null);
-
-                        client.SendData(Packet.ObjectToByteArray(p));
+                        // Отправляем пакет серверу
+                        client.SendData(p);
                         // Создаем обработчики событий приема данных и отключения пользователя
                         client.Received += client_Received;
                         client.Disconnected += client_Disconnected;
+                        // Запускаем асинхронный прием данных от сервера
                         client.ReceiveAsyncStart();                      
                     }
                 }
+                    // Отлавливаем ошибки
                 catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message);
@@ -59,6 +70,7 @@ namespace RemoteFileClient
 
         void client_Disconnected(Client sender)
         {
+            // Отключение
             Disconnect();
         }
 
@@ -68,20 +80,29 @@ namespace RemoteFileClient
             {
                 try
                 {
+                    // Обрабатываем сообщения от сервера
                     switch (p.msg)
                     {
+                            // Тип сообщения "LIST" (получаем список файлов/папок)
                         case MSG_TYPE.LIST:
                             {
+                                // Очищаем список
                                 fileList.Items.Clear();
+                                // Получаем массив байт из пакета и преобразуем набор байт в объект типа список
                                 List<FileSystemInfo> files = (List<FileSystemInfo>)Packet.ByteArrayToObject(p.data);
+                                // Если первый элемент задан (корневой каталог для текущего расположения пользователя на сервере)
                                 if (files[0] != null)
                                 {
+                                    // Создаем новый элемент
                                     ListViewItem li = new ListViewItem("..");
+                                    // Прикрепляем объект файловой информации
                                     li.Tag = files[0];
+                                    // Добавляем в список
                                     fileList.Items.Add(li);
                                 }
+                                // Удаляем информацию о корневом каталоге
                                 files.RemoveAt(0);
-
+                                // Добавляем оставшиеся файлы и папки в список отображения
                                 foreach(var f in files)
                                 {
                                     ListViewItem lvi = new ListViewItem();
@@ -92,13 +113,17 @@ namespace RemoteFileClient
                                 }
                             }
                             break;
+                            // Тип ответа на запрос поиска
                         case MSG_TYPE.SEARCH:
                             {
+                                // Получаем массив строк результатов
                                 string[] searchResult = (string[])Packet.ByteArrayToObject(p.data);
+                                // Если массив строк пуст
                                 if (searchResult.Length == 0)
                                 {
                                     MessageBox.Show("Файл не найден !");
                                 }
+                                    // Иначе выводим все найденные пути для заданного файла
                                 else
                                 {
                                     foreach (var s in searchResult)
@@ -109,8 +134,6 @@ namespace RemoteFileClient
                         default:
                             break;
                     }
-                    
-                    
                 }
                 // Отлавливаем ошибки
                 catch (Exception ex) { MessageBox.Show(ex.Message); Console.WriteLine("Data received: " + ex.Message); }
@@ -119,13 +142,17 @@ namespace RemoteFileClient
 
         void Disconnect()
         {
+            // Отключаемся 
             if (client != null)
             {
+                // Закрываем сокет
                 client.Close();
+                // Вкл/выкл. кнопки управления
                 btn_connect.Enabled = true;
                 btn_disconnect.Enabled = false;
                 btn_search.Enabled = false;
                 txt_file_name.Enabled = false;
+                // Очищаем список файлов
                 fileList.Items.Clear();
             }
         }
@@ -139,13 +166,14 @@ namespace RemoteFileClient
         {
             try
             {
+                // Формируем пакет, выставляем тип сообщения - поиск
                 Packet p = new Packet(MSG_TYPE.SEARCH, Packet.encoding.GetBytes(txt_file_name.Text));
-                int s = client.SendData(Packet.ObjectToByteArray(p));
-                // Получаем размер пакета и отправляем(сигнализирует начало пересылки пакета)
-                // Отправляем пакет                
+                // Отправляем данные
+                int s = client.SendData(p);
                 //if(s > 0)
                     //txt_file_name.Clear();                
             }
+                // Отлавливаем ошибки
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
@@ -154,9 +182,9 @@ namespace RemoteFileClient
 
         private void clientForm_FormClosing(object sender, FormClosingEventArgs e)
         {
+            // Если объект клиента создан - закрываем соединение
             if (client != null)
                 client.Close();
-
         }
 
         private void txt_command_TextChanged(object sender, EventArgs e)
@@ -171,36 +199,21 @@ namespace RemoteFileClient
             {
                 // Получаем выделенный элемент списка
                 ListViewItem lvi = listView.SelectedItems[0];
+                // Получаем файловую инфо прикрепленную к выделоенному элементу списка
                 FileSystemInfo fsi = lvi.Tag as FileSystemInfo;
-
-                
-                
-                // check whether a file is read only
-                bool isReadOnly = ((fsi.Attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly);
-
-                // check whether a file is hidden
-                bool isHidden = ((fsi.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden);
-
-                // check whether a file has archive attribute
-                bool isArchive = ((fsi.Attributes & FileAttributes.Archive) == FileAttributes.Archive);
-
-                // check whether a file is system file
-                bool isSystem = ((fsi.Attributes & FileAttributes.System) == FileAttributes.System);
-
+                // Проверим папка или нет выделена
                 bool isDirectory = ((fsi.Attributes & FileAttributes.Directory) == FileAttributes.Directory);
-
+                // Если папка
                 if (isDirectory)
                 {
+                    // Формируем пакет, тип сообщения - смена каталога
                     Packet p = new Packet(MSG_TYPE.CWD, Packet.encoding.GetBytes(fsi.FullName));
-                    client.SendData(Packet.ObjectToByteArray(p));
+                    // Отправляем пакет
+                    client.SendData(p);
                 }
-
-                /*MessageBox.Show(string.Format("directory {0}, readonly {1}, hidden {2}, archive {3}, system {4}",
-                    isDirectory, isReadOnly, isHidden, isArchive, isSystem));*/
-                
             }
         }
-
+        // Отображаем меню только если есть выделенные элементы
         private void fileSettingsMenu_Opening(object sender, CancelEventArgs e)
         {
             if (fileList.SelectedItems.Count <= 0)
@@ -212,15 +225,19 @@ namespace RemoteFileClient
         {
             if (fileList.SelectedItems.Count == 1)
             {
+                // Получаем выделенный элемент
                 ListViewItem lvi = fileList.SelectedItems[0];
+                // Получаем файловую информацию прикрепленную к выделенному пункту списка
                 FileSystemInfo fsi = lvi.Tag as FileSystemInfo;
+                // Создаем форму для показа аттрибутов файла
                 settingsForm settings = new settingsForm(this);
+                // Задаем название файла 
                 settings.lbl_name.Text = fsi.Name;
+                // Задаем путь к файлу на сервере
                 settings.lbl_full_path.Text = fsi.FullName;
+
                 fileInfo = fsi;
-
-                
-
+                // Получаем аттрибуты
                 bool isDirectory = ((fsi.Attributes & FileAttributes.Directory) == FileAttributes.Directory);
                 bool isArchive = ((fsi.Attributes & FileAttributes.Archive) == FileAttributes.Archive);
                 bool isCompress = ((fsi.Attributes & FileAttributes.Compressed) == FileAttributes.Compressed);
@@ -229,7 +246,7 @@ namespace RemoteFileClient
                 bool isHidden = ((fsi.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden);
                 bool isSystem = ((fsi.Attributes & FileAttributes.System) == FileAttributes.System);
                 bool isReadOnly = ((fsi.Attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly);
-
+                // Запоминаем начальное состояние чекбоксов
                 settings.initState.Add(settings.cb_directory.Name, (isDirectory) ? CheckState.Checked : CheckState.Unchecked);
                 settings.initState.Add(settings.cb_archive.Name, (isArchive) ? CheckState.Checked : CheckState.Unchecked);
                 settings.initState.Add(settings.cb_compressed.Name, (isCompress ) ? CheckState.Checked : CheckState.Unchecked);
@@ -238,8 +255,7 @@ namespace RemoteFileClient
                 settings.initState.Add(settings.cb_hidden.Name, (isHidden ) ? CheckState.Checked : CheckState.Unchecked);
                 settings.initState.Add(settings.cb_system.Name, (isSystem ) ? CheckState.Checked : CheckState.Unchecked);
                 settings.initState.Add(settings.cb_readonly.Name, (isReadOnly ) ? CheckState.Checked : CheckState.Unchecked);
-                
-
+                // Выставляем начальное состояние чекбоксов
                 settings.cb_directory.Checked = isDirectory;
                 settings.cb_archive.Checked = isArchive;
                 settings.cb_compressed.Checked = isCompress;
@@ -248,7 +264,7 @@ namespace RemoteFileClient
                 settings.cb_hidden.Checked = isHidden;
                 settings.cb_system.Checked = isSystem;
                 settings.cb_readonly.Checked = isReadOnly;
-
+                // Прикрепляем к каждому чекбоксу обработчик события изменения состояния
                 settings.cb_directory.CheckStateChanged += settings.cb_CheckStateChanged;
                 settings.cb_archive.CheckStateChanged += settings.cb_CheckStateChanged;
                 settings.cb_compressed.CheckStateChanged += settings.cb_CheckStateChanged;
@@ -257,22 +273,24 @@ namespace RemoteFileClient
                 settings.cb_hidden.CheckStateChanged += settings.cb_CheckStateChanged;
                 settings.cb_system.CheckStateChanged += settings.cb_CheckStateChanged;
                 settings.cb_readonly.CheckStateChanged += settings.cb_CheckStateChanged;
-
-
+                // Если выделен файл
                 if (!isDirectory)
                 {
                     FileInfo fi = (FileInfo)fsi;
+                    // Получим и выведем его размер
                     settings.lbl_size.Text = fi.Length.ToString();
                 }
-
+                // Вызываем показ диалога
                 settings.ShowDialog(this);
-
+                // По закрытии диалога проверяем нужно ли изменять аттрибуты
                 if (apply)
                 {
+                    // Применяем новые аттрибуты
                     fsi.Attributes = fsi.Attributes | fileInfo.Attributes;
+                    // Формируем пакет для смены аттрибутов на сервере
                     Packet p = new Packet(MSG_TYPE.SET_ATTRIBUTE, Packet.ObjectToByteArray(fsi));
-                    byte[] data = Packet.ObjectToByteArray(p);
-                    client.SendData(data);
+                    // Отправляем пакет серверу
+                    client.SendData(p);
                 }
             }
         }
--- RemoteFileAccess/RemoteFileClient/settingsForm.cs
+++ RemoteFileAccess/RemoteFileClient/settingsForm.cs
@@ -11,14 +11,19 @@ namespace RemoteFileClient
 {
     public partial class settingsForm : Form
     {
+        // Используем словарь для отслеживания изменений относительно начального состояния отмеченных свойств файла/папки
         public Dictionary<string, CheckState> initState = new Dictionary<string, CheckState>();
+        // Список чекбоксов
         private List<CheckBox> boxes = new List<CheckBox>();
+        // Объект главной формы
         clientForm form;
 
         public settingsForm(clientForm form)
         {
             InitializeComponent();
+            // Получаем объект главной формы
             this.form = form;
+            // Собираем список всех чекбоксов
             boxes.Add(cb_directory);
             boxes.Add(cb_archive);
             boxes.Add(cb_compressed);
@@ -28,32 +33,40 @@ namespace RemoteFileClient
             boxes.Add(cb_system);
             boxes.Add(cb_readonly);
         }
-        
-
+        // Обработчик изменения состояния чекбокса
         public void cb_CheckStateChanged(object sender, EventArgs e)
         {
-            form.apply = false;
+            // Выставляем маркер применять изменения или нет 
+            form.apply = false;// не применять
+            // Получаем идентификатор чекбокса сгенерировавшего событие изминения состояния (снята галочка или отмечена)
             CheckBox cb = sender as CheckBox;
+            // Пробегаемся по всем чекбоксам формы
             foreach (var b in boxes)
             {
+                // Проверяем состояние чекбокса с именем [Name], если текущее состояние не совподает с тем которое было задано при создании
                 if (initState[b.Name] != b.CheckState)
                 {
+                    // тогда выставляем маркер в "применить изменения"
                     form.apply = true;
+                    // выходим из перечисления если хотя бы один чекбокс изменил состояние
                     break;
                 }
             }
+            // Включаем или выключаем кнопку применения изменения для выбранного файла в зависимости от маркер "apply"
             btn_apply.Enabled = form.apply;
         }
-
+        
         private void btn_cancel_Click(object sender, EventArgs e)
         {
-            //form.apply = false;
+            form.apply = false;
+            // Закрываем форму
             Close();
         }
 
         private void btn_apply_Click(object sender, EventArgs e)
         {
-            //form.apply = false;
+            form.apply = true;
+            // Собираем все атрибуты которые были отмечены галочками
             form.fileInfo.Attributes = System.IO.FileAttributes.Normal;
             if (cb_directory.Checked)
                 form.fileInfo.Attributes = form.fileInfo.Attributes | System.IO.FileAttributes.Directory;
@@ -71,7 +84,7 @@ namespace RemoteFileClient
                 form.fileInfo.Attributes = form.fileInfo.Attributes | System.IO.FileAttributes.ReadOnly;
             if (cb_normal.Checked)
                 form.fileInfo.Attributes = form.fileInfo.Attributes | System.IO.FileAttributes.Normal;
-            form.apply = true;
+            // Закрываем форму
             Close();
         }
     }
